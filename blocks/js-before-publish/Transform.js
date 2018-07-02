8032b3296160c832b58aae5e251d94d8ec7c7c10
Jeremy Pollock <jeremy@jeremy-pollock.local>
2018-07-02 15:59:36 -0700

    updating for using k/v store

--- blocks/js-before-publish/Transform.js
+++ blocks/js-before-publish/Transform.js
@@ -1,9 +1,26 @@
 export default (request) => { 
     const kvstore = require('kvstore');
     const xhr = require('xhr');
-    request.message.eon = {'bg': request.message.entries[0].sgv, 'trend':  request.message.entries[0].trend, 'x': request.message.entries[0].date};
-    //console.log('request',request.message.entries[0].sgv); // Log the request envelope passed 
-    //request.eon = 
-    //console.log('request',request["entries"][0]); // Log the request envelope passed 
-    return request.ok(); // Return a promise when you're done 
+    
+    var current_bg = request.message.entries[0].sgv;
+    return kvstore.get("last_100_entries").then((last_100_entries_new) => {
+        last_100_entries_new = ""; 
+        if (!last_100_entries) {
+            last_100_entries_new = last_100_entries;
+        }
+        last_100_entries_new += bg + ",";
+        
+        kvstore.set("last_100_entries", last_100_entries_new).catch(
+
+                
+        );
+
+        console.log(last_100_entries_new);
+        request.message.eon = {'bg': current_bg, 'trend':  request.message.entries[0].trend, 'x': request.message.entries[0].date};
+        return request.ok(); // Return a promise when you're done         
+    });
+
+//    
+
+    
 }
